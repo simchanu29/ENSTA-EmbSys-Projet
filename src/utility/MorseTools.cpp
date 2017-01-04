597c2be9f607b727206beb419f29a69c56b30580
FadilX <ennouhifadil@gmail.com>
2017-01-04 17:24:42 +0100

    Petite fonction Morse

--- src/utility/MorseTools.cpp
+++ src/utility/MorseTools.cpp
@@ -1,5 +1,31 @@
-//
-// Created by tag on 03/01/17.
-//
+#include <iostream>
+#include <string>
 
-#include "MorseTools.h"
+std::string morsiphy(std::string myString){
+
+    std::string latin;
+    char latinAlphabet[29] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','.',',',' '};
+    std::string morseAlphabet[30] = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".-.-.-", "--..--" };
+    std::string morse;
+    
+    latin=myString;
+
+    for (unsigned i = 0; i<latin.length(); ++i)
+    {
+        for (int counter = 0; counter < 30; counter++)
+        {
+                if (latin.at(i) == latinAlphabet[counter])
+                {
+                    std::cout << morseAlphabet[counter] << "|";
+                    break;
+                }
+        }
+    }
+        
+return morse;
+   }
+int main()
+{
+	std::string result=morsiphy("kaaris");
+	std::cout<<result;
+}
