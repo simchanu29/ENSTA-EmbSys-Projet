eb9136200591bb8ca17b4cfaad1964e06b5f684e
simchanu29 <simon.chanu@laposte.net>
2017-01-10 16:15:27 +0100

    CMakeLists C++11

--- src/tests/unitTest_tools.cpp
+++ src/tests/unitTest_tools.cpp
@@ -4,15 +4,19 @@
 
 #include "../config.h"
 
+#include <vector>
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    std::string txt = "test morse";
-    std::string msg = CryptoTools::cryptCesar(txt,13);
-    printf("%s\n",txt.c_str());
-    printf("%s\n",msg.c_str());
-    std::string result=MorseTools::morsiphy("kaaris");
-    std::cout<<result;
-    fflush(stdout);
+//    std::string txt = "test morse";
+//    std::string msg = CryptoTools::cryptCesar(txt,13);
+//    printf("%s\n",txt.c_str());
+//    printf("%s\n",msg.c_str());
+//    std::string result=MorseTools::morsiphy("kaaris");
+//    std::cout<<result;
+//    fflush(stdout);
+
+    std::vector<std::string> str = {"test","test"};
+
 }
