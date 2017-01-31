9db43b4f88981bc5c1d2ffd08733c9a6fd69d700
simchanu29 <simon.chanu@laposte.net>
2017-01-31 23:03:41 +0100

    raspberry.cmake

---
+++ toolchains/raspberry.cmake
@@ -0,0 +1,21 @@
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_VERSION 1)
+
+SET(DEVROOT ${CMAKE_CURRENT_SOURCE_DIR}/toolchains/raspberry)
+SET(PIROOT ${DEVROOT})
+SET(PITOOLS ${DEVROOT})
+
+SET(TOOLROOT ${PITOOLS}/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   ${TOOLROOT}/bin/arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER ${TOOLROOT}/bin/arm-linux-gnueabihf-g++)
+
+SET(CMAKE_SYSROOT ${PIROOT})
+SET(CMAKE_FIND_ROOT_PATH ${PIROOT})
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
